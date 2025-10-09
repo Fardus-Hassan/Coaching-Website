2949bb2a3516374ad9057a6d8edb7b1051ebb87f
Fardus-Hassan <webdev.fardus@gmail.com>
2025-10-09 15:06:50 +0600

    added dynamic title and icon in site tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,39 @@ import "./globals.css";
 import Navbar from "@/components/Utility/Navbar";
 import Footer from "@/components/Utility/Footer";
 import ScrollToTop from "@/components/Utility/ScrollToTop";
+import { useGetInstitutesQuery } from "@/redux/features/api/institute/instituteApi";
 
-export const metadata = {
-  title: "Coaching",
-  description: "Coaching Management System",
-  icons: {
-    icon: [{ url: "/icon2.png" }],
-  },
-};
+export async function generateMetadata() {
+  try {
+    const res = await fetch("https://coaching.attendclub.top/api/institutes/", {
+      cache: "no-store",
+    });
+    const data = await res.json();
+    const institute = data[0];
+
+    return {
+      title: institute?.institute_name || "Coaching",
+      description: "Coaching Management System",
+      icons: {
+        icon: [
+          {
+            url: institute?.institute_logo || "/icon2.png",
+            type: "image/png",
+          },
+        ],
+      },
+    };
+  } catch {
+    return {
+      title: "Coaching",
+      description: "Coaching Management System",
+      icons: {
+        icon: [{ url: "/icon2.png" }],
+      },
+    };
+  }
+}
+
 
 export default function RootLayout({
   children,
