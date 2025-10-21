efcbce2f90f23d4b899430d6d465356d247e7472
Fardus-Hassan <webdev.fardus@gmail.com>
2025-10-21 21:35:39 +0600

    check

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,52 @@
+// import baseUrl from "@/apiConfig";
+// import Image from "next/image";
+
+// export default async function Loading() {
+//   try {
+//     const res = await fetch(`${baseUrl()}institutes/`, {
+//       cache: "no-store",
+//     });
+
+//     const data = await res.json();
+//     const institute = data?.[0];
+
+//     return (
+//       <div className="fixed inset-0 flex flex-col items-center justify-center bg-white text-white z-50">
+//         <Image
+//           src={institute?.institute_logo || "/icon2.png"}
+//           alt={institute?.institute_name || "Logo"}
+//           width={100}
+//           height={100}
+//           className="animate-bounce"
+//           priority
+//         />
+//         <p className="mt-3 text-sm text-[var(--color-text)] animate-pulse">
+//           {institute?.institute_name || "Loading..."}
+//         </p>
+//       </div>
+//     );
+//   } catch (error) {
+//     return (
+//       <div className="fixed inset-0 flex flex-col items-center justify-center bg-white text-white z-50">
+//         <Image
+//           src="/icon2.png"
+//           alt="Logo"
+//           width={100}
+//           height={100}
+//           className="animate-bounce"
+//           priority
+//         />
+//         <p className="mt-3 text-sm text-[var(--color-text)] animate-pulse">
+//           Coaching Management System
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+
 import { Loader2 } from "lucide-react";
 
 export default async function Loading() {
